1f88593b7309e169ffe03c0d428a17a704c3321c
Ali Hassan <yali101@email.arizona.edu>
2016-02-22 23:15:02 -0700

    part
    done

---
+++ lcd.h
@@ -0,0 +1,23 @@
+/*  
+ *  Class: ECE372a
+ * 
+ *  Group: 209
+ *
+ *  Lab: 1 Part 2
+ * 
+ *  File: lcd.c
+ * 
+ *  Member: Michael Harmon (Software)
+ *          Abdul Rana (Hardware)
+ *          James Beulke (System Integration)
+ *          Ali Hassan (Quality Control)
+ */
+
+void writeLCD(unsigned char word, unsigned int commandType, unsigned int delay);
+void writeFourBits(unsigned char word, unsigned int commandType, unsigned int delayAfter, unsigned int lower);
+void initLCD(void);
+void clearLCD(void);
+void moveCursorLCD(unsigned char x, unsigned char y);
+void printCharLCD(char c);
+void printStringLCD(const char* s);
+void testLCD();
